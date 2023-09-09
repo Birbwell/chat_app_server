ea94661a8e9ddb2e9640437b19d87ede583aafb2
Aeskull <aeskulstyr@gmail.com>
2023-09-09 16:25:13 -0600

    somethingsomething

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
-    let (tx_master, mut rx_master) = channel::<String>(25);
+    let (tx_master, mut rx_master) = channel::<(Vec<u8>, Vec<u8>)>(25);
     let sv_rsa = Rsa::generate(2048)?;
     let rsa_arc = Arc::new(sv_rsa);
     let tx_arc = Arc::new(tx_master);
@@ -40,10 +40,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut tasks = vec![];
 
     loop {
+        eprintln!("Bing");
         tokio::select! {
             Ok((stream, addr)) = listener.accept() => {
                 println!("Incoming connection from {addr}");
-                let (tx, rx) = channel::<String>(25);
+                let (tx, rx) = channel::<(Vec<u8>, Vec<u8>)>(25);
                 let tx_clone = Arc::clone(&tx_arc);
                 let rsa_clone = Arc::clone(&rsa_arc);
 
@@ -54,7 +55,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 senders.push(tx);
             },
             Some(msg) = rx_master.recv() => {
-                writeln!(f, "Message Recieved: {msg}")?;
+                writeln!(f, "Message Recieved: {msg:?}")?;
                 let mut rms = vec![];
                 for (idx, sender) in senders.iter().enumerate() {
                     let Ok(_) = sender.send(msg.clone()).await else {
@@ -83,35 +84,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn handle_connection(
     mut stream: TcpStream,
     addr: SocketAddr,
-    mut rx: Receiver<String>,
-    tx: Arc<Sender<String>>,
+    mut rx: Receiver<(Vec<u8>, Vec<u8>)>,
+    tx: Arc<Sender<(Vec<u8>, Vec<u8>)>>,
     sv_rsa: Arc<Rsa<Private>>,
 ) {
     let mut cl_rsa: Option<Rsa<Public>> = None;
     loop {
         let mut typ_buf = [0u8; 3];
         let mut len_buf = [0u8; 4];
-        eprintln!("Bing");
         tokio::select! {
             result = stream.read_exact(&mut typ_buf) => {
                 match result {
                     Ok(0) => break,
                     Ok(_) => {
-                        let typ = String::from_utf8_lossy(&typ_buf).to_string();
                         stream.read_exact(&mut len_buf).await.unwrap();
-                        match typ.as_str() {
-                            "MSG" => {
-                                let msg_len = u32::from_be_bytes(len_buf) as usize;
-                                let mut msg_buf = vec![0u8; msg_len];
-                                if stream.read_exact(&mut msg_buf).await.is_err() {
-                                    break;
+                        match &typ_buf {
+                            b"ENC" => {
+                                if cl_rsa.is_some() {
+                                    let key_len = u32::from_be_bytes(len_buf) as usize;
+                                    let mut key = vec![0u8; key_len];
+                                    stream.read_exact(&mut key).await.unwrap();
+
+                                    let mut msg_hed = [0u8; 3];
+                                    stream.read_exact(&mut msg_hed).await.unwrap();
+
+                                    if &msg_hed == b"MSG" {
+                                        let mut msg_len_buf = [0u8; 4];
+                                        stream.read_exact(&mut msg_len_buf).await.unwrap();
+                                        let msg_len = u32::from_be_bytes(msg_len_buf) as usize;
+                                        let mut msg = vec![0u8; msg_len];
+                                        stream.read_exact(&mut msg).await.unwrap();
+
+                                        let dec_key = {
+                                            let mut t = [0u8; 2048];
+                                            let len = sv_rsa.private_decrypt(&key, &mut t, Padding::PKCS1).unwrap();
+                                            t[0..len].to_owned()
+                                        };
+                                        tx.send((dec_key, msg)).await.unwrap();
+                                    } else {
+                                        eprintln!("An error has occured on connection from {addr:?}");
+                                        break;
+                                    }
                                 }
-                                let mut dec_msg: Vec<u8> = vec![];
-                                sv_rsa.private_decrypt(&msg_buf, &mut dec_msg, Padding::PKCS1).unwrap();
-                                let msg = String::from_utf8_lossy(&dec_msg).to_string();
-                                tx.send(msg).await.unwrap();
                             },
-                            "KEY" => { // Works
+                            b"PUB" => {
                                 let key_len = u32::from_be_bytes(len_buf) as usize;
                                 let mut key_buf = vec![0u8; key_len];
                                 if stream.read_exact(&mut key_buf).await.is_err() {
@@ -128,25 +144,20 @@ async fn handle_connection(
                     }
                 }
             },
-            Some(msg) = rx.recv() => {
-                eprintln!("{msg:?}");
-                let msg_len = msg.len();
-                len_buf = u32::to_be_bytes(msg_len as u32);
-                let full_msg = [&len_buf, msg.as_bytes()].concat();
-                if let Err(e) = stream.write_all(&full_msg).await {
-                    eprintln!("ERROR: {e:?}");
-                    _ = stream.shutdown().await;
-                    return;
-                };
+            Some((key, msg)) = rx.recv() => {
+                if let Some(c_rsa) = cl_rsa.as_ref() {
+                    let enc_key = {
+                        let mut t = vec![0u8; 2048 as usize];
+                        let len = c_rsa.public_encrypt(&key, &mut t, Padding::PKCS1).unwrap();
+                        t[0..len].to_owned()
+                    };
+                    let key_hed = "ENC".as_bytes();
+                    let msg_hed = "MSG".as_bytes();
 
-                if let Some(r) = cl_rsa.as_ref() {
-                    let mut enc_msg = vec![];
-                    let msg_bytes = msg.as_bytes();
-                    r.public_encrypt(msg_bytes, &mut enc_msg, Padding::PKCS1).unwrap();
-                    let msg_len = (enc_msg.len() as u32).to_be_bytes(); // Write the length header.
-                    let header = "MSG".as_bytes();
-                    stream.write_all(&[header, &msg_len, &enc_msg].concat()).await.unwrap();
-                    stream.flush().await.unwrap();
+                    let key_len = (enc_key.len() as u32).to_be_bytes();
+                    let msg_len = (msg.len() as u32).to_be_bytes();
+
+                    stream.write_all(&[key_hed, &key_len, &enc_key, msg_hed, &msg_len, &msg].concat()).await.unwrap();
                 }
             }
         }
