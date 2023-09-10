b6d4c35fc59930cef053ef0df459d3d76b351a89
Aeskull <aeskulstyr@gmail.com>
2023-09-09 21:53:38 -0600

    working encryption. may have to go back and make it more secure though

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,47 @@
-use std::{io::Write, net::SocketAddr, sync::Arc, time::Duration};
+use std::{fs::File, io::Write, net::SocketAddr, sync::Arc, time::Duration};
 
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    sync::mpsc::{channel, Receiver, Sender},
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Mutex,
+    },
 };
 
-use openssl::pkey::{Public, Private};
-use openssl::rsa::{Rsa, Padding};
+use openssl::pkey::Private;
+use openssl::rsa::{Padding, Rsa};
+use openssl::symm::{encrypt, Cipher};
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
 
 const IP: &str = "0.0.0.0:42530";
+const SYMM: usize = 32;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut f = std::fs::OpenOptions::new()
-        .append(true)
-        .write(true)
-        .create(true)
-        .open("Log.txt")?;
+    #[cfg(debug_assertions)]
+    let f = Arc::new(Mutex::new(
+        std::fs::OpenOptions::new()
+            .append(true)
+            .write(true)
+            .create(true)
+            .open("latest.log")?,
+    ));
     let listener = TcpListener::bind(IP).await?;
     println!("Listening on {IP}");
 
-    let mut check = tokio::task::spawn_blocking(|| {
-        loop {
-            if let Ok(Event::Key(KeyEvent { code: KeyCode::Esc, .. })) = event::read() {
-                return
-            }
-            std::thread::sleep(Duration::from_millis(1));
+    let mut check = tokio::task::spawn_blocking(|| loop {
+        if let Ok(Event::Key(KeyEvent {
+            code: KeyCode::Esc, ..
+        })) = event::read()
+        {
+            return;
         }
+        std::thread::sleep(Duration::from_millis(1));
     });
 
-    let (tx_master, mut rx_master) = channel::<(Vec<u8>, Vec<u8>)>(25);
+    let (tx_master, mut rx_master) = channel::<Vec<u8>>(25);
     let sv_rsa = Rsa::generate(2048)?;
     let rsa_arc = Arc::new(sv_rsa);
     let tx_arc = Arc::new(tx_master);
@@ -40,22 +49,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut tasks = vec![];
 
     loop {
-        eprintln!("Bing");
         tokio::select! {
             Ok((stream, addr)) = listener.accept() => {
                 println!("Incoming connection from {addr}");
-                let (tx, rx) = channel::<(Vec<u8>, Vec<u8>)>(25);
+                let (tx, rx) = channel::<Vec<u8>>(25);
                 let tx_clone = Arc::clone(&tx_arc);
                 let rsa_clone = Arc::clone(&rsa_arc);
 
+                #[cfg(debug_assertions)]
+                let f_clone = Arc::clone(&f);
+
                 tasks.push((tokio::spawn(async move {
+                    #[cfg(debug_assertions)]
+                    handle_connection(stream, addr, rx, tx_clone, rsa_clone, f_clone).await;
+                    #[cfg(not(debug_assertions))]
                     handle_connection(stream, addr, rx, tx_clone, rsa_clone).await;
                 }), addr));
 
                 senders.push(tx);
             },
             Some(msg) = rx_master.recv() => {
-                writeln!(f, "Message Recieved: {msg:?}")?;
                 let mut rms = vec![];
                 for (idx, sender) in senders.iter().enumerate() {
                     let Ok(_) = sender.send(msg.clone()).await else {
@@ -73,8 +86,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         task.0.abort();
                         println!("Connection from {} has been closed", task.1);
                     }
-                    println!("Server shutting down");
                 }
+                println!("Server shutting down");
                 break Ok(());
             },
         }
@@ -84,11 +97,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn handle_connection(
     mut stream: TcpStream,
     addr: SocketAddr,
-    mut rx: Receiver<(Vec<u8>, Vec<u8>)>,
-    tx: Arc<Sender<(Vec<u8>, Vec<u8>)>>,
+    mut rx: Receiver<Vec<u8>>,
+    tx: Arc<Sender<Vec<u8>>>,
     sv_rsa: Arc<Rsa<Private>>,
+    #[cfg(debug_assertions)] f: Arc<Mutex<File>>,
 ) {
-    let mut cl_rsa: Option<Rsa<Public>> = None;
+    let ciph = Cipher::aes_256_cbc();
+    let mut first = true;
     loop {
         let mut typ_buf = [0u8; 3];
         let mut len_buf = [0u8; 4];
@@ -100,43 +115,49 @@ async fn handle_connection(
                         stream.read_exact(&mut len_buf).await.unwrap();
                         match &typ_buf {
                             b"ENC" => {
-                                if cl_rsa.is_some() {
-                                    let key_len = u32::from_be_bytes(len_buf) as usize;
-                                    let mut key = vec![0u8; key_len];
-                                    stream.read_exact(&mut key).await.unwrap();
+                                let key_len = u32::from_be_bytes(len_buf) as usize;
+                                let mut key = vec![0u8; key_len];
+                                stream.read_exact(&mut key).await.unwrap();
 
-                                    let mut msg_hed = [0u8; 3];
-                                    stream.read_exact(&mut msg_hed).await.unwrap();
+                                let mut msg_len_buf = [0u8; 4];
+                                stream.read_exact(&mut msg_len_buf).await.unwrap();
+                                let msg_len = u32::from_be_bytes(msg_len_buf) as usize;
+                                let mut msg = vec![0u8; msg_len];
+                                stream.read_exact(&mut msg).await.unwrap();
 
-                                    if &msg_hed == b"MSG" {
-                                        let mut msg_len_buf = [0u8; 4];
-                                        stream.read_exact(&mut msg_len_buf).await.unwrap();
-                                        let msg_len = u32::from_be_bytes(msg_len_buf) as usize;
-                                        let mut msg = vec![0u8; msg_len];
-                                        stream.read_exact(&mut msg).await.unwrap();
-
-                                        let dec_key = {
-                                            let mut t = [0u8; 2048];
-                                            let len = sv_rsa.private_decrypt(&key, &mut t, Padding::PKCS1).unwrap();
-                                            t[0..len].to_owned()
-                                        };
-                                        tx.send((dec_key, msg)).await.unwrap();
-                                    } else {
-                                        eprintln!("An error has occured on connection from {addr:?}");
-                                        break;
-                                    }
-                                }
+                                tx.send([typ_buf.as_slice(), &len_buf, &key, &msg_len_buf, &msg].concat()).await.unwrap();
                             },
-                            b"PUB" => {
+                            b"PUB" if first => {
                                 let key_len = u32::from_be_bytes(len_buf) as usize;
                                 let mut key_buf = vec![0u8; key_len];
                                 if stream.read_exact(&mut key_buf).await.is_err() {
                                     break;
                                 }
-                                cl_rsa = Some(Rsa::public_key_from_der(&key_buf).unwrap());
+                                let cl_r = Rsa::public_key_from_der(&key_buf).unwrap();
+
+                                let sv_prv = sv_rsa.private_key_to_der().unwrap();
+                                // Encrypt with symm key, encrypt symm key with rsa
+                                let symm = gen_rand_symm(SYMM);
+                                let enc_symm = {
+                                    let mut t = vec![0u8; sv_prv.len()];
+                                    let len = cl_r.public_encrypt(&symm, &mut t, Padding::PKCS1).unwrap();
+                                    t[0..len].to_owned()
+                                };
+
+                                let prv_hed = "PRV".as_bytes();
+                                let symm_len = (enc_symm.len() as u32).to_be_bytes();
+                                let enc_prv = encrypt(ciph, &symm, None, &sv_prv).unwrap();
+
+                                let prv_len = (enc_prv.len() as u32).to_be_bytes();
+                                stream.write_all(&[prv_hed, &symm_len, &enc_symm, &prv_len, &enc_prv].concat()).await.unwrap();
+
+                                first = false;
                             },
                             _ => eprintln!("Error reading from stream: {result:?}"),
                         }
+                    },
+                    Err(e) if e.kind() == tokio::io::ErrorKind::UnexpectedEof => {
+                        break;
                     },
                     Err(e) => {
                         eprintln!("Error reading from client: {e:?}");
@@ -144,24 +165,25 @@ async fn handle_connection(
                     }
                 }
             },
-            Some((key, msg)) = rx.recv() => {
-                if let Some(c_rsa) = cl_rsa.as_ref() {
-                    let enc_key = {
-                        let mut t = vec![0u8; 2048 as usize];
-                        let len = c_rsa.public_encrypt(&key, &mut t, Padding::PKCS1).unwrap();
-                        t[0..len].to_owned()
-                    };
-                    let key_hed = "ENC".as_bytes();
-                    let msg_hed = "MSG".as_bytes();
-
-                    let key_len = (enc_key.len() as u32).to_be_bytes();
-                    let msg_len = (msg.len() as u32).to_be_bytes();
-
-                    stream.write_all(&[key_hed, &key_len, &enc_key, msg_hed, &msg_len, &msg].concat()).await.unwrap();
+            Some(msg) = rx.recv() => {
+                #[cfg(debug_assertions)]
+                {
+                    let mut fl = f.lock().await;
+                    for u in &msg {
+                        write!(fl, "{u:08b}").unwrap();
+                    }
+                    writeln!(fl).unwrap();
                 }
+                stream.write_all(&msg).await.unwrap();
             }
         }
     }
 
     println!("Connection from {addr} has been closed");
+}
+
+fn gen_rand_symm(prec: usize) -> Vec<u8> {
+    let mut key = vec![0u8; prec];
+    openssl::rand::rand_bytes(&mut key).unwrap();
+    key
 }
